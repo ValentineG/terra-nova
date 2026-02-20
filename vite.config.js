6d8df0784ccdee9708cbd7fdf30a14eb085723e9
Valentyn <vdgnatyuk@gmail.com>
2026-02-20 09:42:26 +0200

    new section Infra with slider. Refactoring

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ export default defineConfig({
   plugins: [
     injectDiff(),
   ],
+  build: {
+    cssCodeSplit: true, // Дозволяє розділяти CSS
+    rollupOptions: {
+      output: {
+        // Це змусить зберігати назви, але додавати хеш
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            // Зберігаємо структуру: css/назва.хеш.css
+            return 'css/[name].[hash][extname]';
+          }
+          return 'assets/[name].[hash][extname]';
+        },
+      },
+    },
+  },
 });
