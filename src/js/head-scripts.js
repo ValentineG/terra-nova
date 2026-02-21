6818fd0216403741b06d3fe00c8ada3adefe348a
Valentyn <vdgnatyuk@gmail.com>
2026-02-21 21:08:41 +0200

    Apartments new section

--- src/js/head-scripts.js
+++ src/js/head-scripts.js
@@ -154,3 +154,32 @@ jQuery(document).ready(function($) {
         ]
     });
 });
+
+// Слайдер для розділу "Our Apartments"
+jQuery(document).ready(function($) {
+    $('.apartments-slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false, // Кнопки не потрібні за вашою умовою
+        dots: false,
+        draggable: true, // Дозволяє "свайпати" мишкою
+        swipe: true,     // Дозволяє свайпати на тачскрінах
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
